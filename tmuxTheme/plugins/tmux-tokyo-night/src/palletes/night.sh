a61428af01255eaefa0ed24a66d64e46edc4a0dd
ASR-CuriousOne <stark.the.avenger.2006@gmail.com>
2026-02-11 23:20:57 +0530

    Added tmux theme

---
+++ tmuxTheme/plugins/tmux-tokyo-night/src/palletes/night.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#Tokyo Night Pallet
+declare -A PALLETE=(
+  [none]="NONE"
+  [bg]="#1a1b26"
+  [bg_dark]="#16161e"
+  [bg_highlight]="#292e42"
+  [terminal_black]="#414868"
+  [fg]="#c0caf5"
+  [fg_dark]="#a9b1ff"
+  [fg_gutter]="#172026"
+  [dark3]="#545c7e"
+  [comment]="#565f89"
+  [dark5]="#737aa2"
+  [blue0]="#5698d1"
+  [blue]="#7aa2f7"
+  [cyan]="#7dcfff"
+  [blue1]="#2ac3de"
+  [blue2]="#0db9d7"
+  [blue5]="#000000"
+  [blue6]="#b4f9f8"
+  [blue7]="#394b70"
+  [magenta]="#5698D1"
+  [magenta2]="#ff007c"
+  [purple]="#5fabed"
+  [orange]="#ff9e64"
+  [yellow]="#F5787E"
+  [green]="#D9946C"
+  [green1]="#73daca"
+  [green2]="#41a6b5"
+  [teal]="#1abc9c"
+  [red]="#f7768e"
+  [red1]="#db4b4b"
+  [white]="#ffffff"
+  [gray]="#172026"
+)
+
+export PALLETE
+
